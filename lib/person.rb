d69d0944b0583445dc227968b77d2c405a031571
wandiamugo <wwandia173@gmail.com>
2022-08-17 16:03:19 +0300

    Done

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,32 @@
 class Person
 
-end
+    # setter method
+    def name=(name)
+      @name = name
+    end
+  
+    # getter method
+    def name
+      @name
+    end
+  
+  end
+
+kanye = Person.new
+kanye.name = "Kanye"
+
+class Person
+
+    # setter method
+    def job=(job)
+      @job = job
+    end
+  
+    # getter method
+    def job
+      @job
+    end
+  
+  end
+beyonce =Person.new
+beyonce.job = "singer"

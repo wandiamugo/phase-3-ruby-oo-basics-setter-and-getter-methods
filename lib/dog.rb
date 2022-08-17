704b16e65ba862879a4064ff8a8c5d8d5357f0cc
wandiamugo <wwandia173@gmail.com>
2022-08-17 16:03:03 +0300

    commit tree 89d2fbc2be1563b8104fbfabc1d758b3b21dea58

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,33 @@
 class Dog
 
-end
+    # setter method
+    def name=(name)
+      @name = name
+    end
+  
+    # getter method
+    def name
+      @name
+    end
+  
+  end
+
+fido =Dog.new
+fido.name = "Fido"
+
+class Dog
+
+    # setter method
+    def breed=(breed)
+      @breed = breed
+    end
+  
+    # getter method
+    def breed
+      @breed
+    end
+  
+  end
+
+  snoopy = Dog.new
+  snoopy.breed = "Beagle"
